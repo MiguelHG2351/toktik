567d1a5f7fb16f1bc71ae6427ae5c2d030242864
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-01-15 16:44:47 -0600

    feat: implementations of datasources and respositories in the infraestructure layer

---
+++ lib/infrastructure/respositories/video_posts_repository_impl.dart
@@ -0,0 +1,22 @@
+import 'package:toktik/domain/datasource/video_post_datasource.dart';
+import 'package:toktik/domain/entities/video_post.dart';
+import 'package:toktik/domain/repositories/video_post_repository.dart';
+
+class VideoPostsRepository implements VideoPostRepository {
+  final VideoPostDatasource videosDatasource;
+
+  VideoPostsRepository({
+    required this.videosDatasource
+  });
+  
+  @override
+  Future<List<VideoPost>> getFavoriteVideosByUser(String userID) {
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<VideoPost>> getTrendingVideosByPage(int page) {
+    return videosDatasource.getTrendingVideosByPage(page);
+  }
+
+}
