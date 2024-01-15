7359ed8d9e2a46bf4a0b513f25f185feaffef0c9
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2024-01-15 08:49:25 -0600

    added domain and infrastructure settings for initial state data

--- lib/main.dart
+++ lib/main.dart
@@ -10,14 +10,14 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return  MultiProvider(
       providers: [
         ChangeNotifierProvider<DiscoverProvider>(
-          create: (_) => DiscoverProvider(),
+          create: (_) => DiscoverProvider()..loadNextPage(),
+          lazy: false,
         ),
       ],
       child: MaterialApp(
